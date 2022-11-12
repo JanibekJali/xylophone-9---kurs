e1c835eaa29f6d6eabf3e4c8e845e7a6f8f3c0b4
User <user@Users-MacBook-Pro.local>
2022-11-12 07:55:34 +0600

    xylophone buttu

--- lib/ui/welcome_ui.dart
+++ lib/ui/welcome_ui.dart
@@ -12,52 +12,183 @@ class WelcomeUI extends StatelessWidget {
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            newMethod(Colors.pink, 'Do', Colors.white),
-            newMethod(Colors.red, 'Re', Colors.amber),
-            newMethod(Colors.blue, 'Me', Colors.red),
-            newMethod(Colors.black, 'Fa', Colors.red),
-            newMethod(Colors.teal, 'Sol', Colors.red),
-            newMethod(Colors.green, 'Lya', Colors.red),
-            newMethod(Colors.orange, 'Si', Colors.red),
-            newMethod(Colors.pink, 'Do', Colors.red),
-            Expanded(
-              child: InkWell(
-                onTap: () {
-                  AssetsAudioPlayer.newPlayer().open(
-                    Audio("assets/notes/note_1.wav"),
-                    autoStart: true,
-                    showNotification: true,
-                  );
-                  // final AudioPlayer audioPlayer = AudioPlayer();
-                  // audioPlayer.play('assets/notes/note_1.wav');
-                },
-                child: Container(
-                  width: double.infinity,
-                  color: Colors.white,
-                  child: const Text(
-                    'misal',
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+          children: const [
+            // newMethod1(
+            //     containerTus: Colors.teal,
+            //     text: 'Do',
+            //     textColor: Colors.white,
+            //     noteNumber: '1'),
+            // newMethod1(
+            //     containerTus: Colors.red,
+            //     text: 'Re',
+            //     textColor: Colors.amber,
+            //     noteNumber: '2'),
+            // newMethod1(
+            //     containerTus: Colors.black,
+            //     text: 'Me',
+            //     textColor: Colors.amber,
+            //     noteNumber: '3'),
+            // newMethod1(
+            //     containerTus: Colors.green,
+            //     text: 'Fa',
+            //     textColor: Colors.white,
+            //     noteNumber: '4'),
+            // newMethod1(
+            //     containerTus: Colors.pink,
+            //     text: 'Sol',
+            //     textColor: Colors.amber,
+            //     noteNumber: '2'),
+            // newMethod1(
+            //     containerTus: Colors.blue,
+            //     text: 'Lya',
+            //     textColor: Colors.amber,
+            //     noteNumber: '5'),
+            // newMethod1(
+            //     containerTus: Colors.green,
+            //     text: 'Si',
+            //     textColor: Colors.amber,
+            //     noteNumber: '6'),
+            // newMethod1(
+            //     containerTus: Colors.white,
+            //     text: 'Do',
+            //     textColor: Colors.amber,
+            //     noteNumber: '7'),
+            // // newMethod2(Colors.blue, 'Me', Colors.red),
+            // newMethod3(Colors.teal),
+            // newMethod(Colors.teal, 'Sol', Colors.red),
+            // newMethod(Colors.green, 'Lya', Colors.red),
+            // newMethod(Colors.orange, 'Si', Colors.red),
+            // newMethod(Colors.pink, 'ef', Colors.red),
+            NotaWidget(containerTus: Colors.amber),
+            NotaWidget(containerTus: Colors.red),
+            NotaWidget(containerTus: Colors.pink),
           ],
         ),
       ),
     );
   }
 
-  Expanded newMethod(Color tus, String text, Color color) {
+  // {} - Aty bar metod, berbei kosok dele bolot
+  Expanded newMethod1({
+    Color? containerTus,
+    String? text,
+    @required Color? textColor,
+    String? noteNumber,
+  }) {
     return Expanded(
-      child: Container(
-        width: double.infinity,
-        color: tus,
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 30, color: color),
+      child: InkWell(
+        onTap: () {
+          AssetsAudioPlayer.newPlayer().open(
+            Audio("assets/notes/note_$noteNumber.wav"),
+            autoStart: true,
+            showNotification: true,
+          );
+          // final AudioPlayer audioPlayer = AudioPlayer();
+          // audioPlayer.play('assets/notes/note_1.wav');
+        },
+        child: Container(
+          width: double.infinity,
+          color: containerTus,
+          child: Text(
+            'Note:$text ',
+            style: TextStyle(
+              fontSize: 30,
+              color: textColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // () - aty jok metod, birok sozsuz berish kerek
+  // Expanded newMethod2(Color? containerTus, String? text, Color? textColor, ) {
+  //   return Expanded(
+  //     child: InkWell(
+  //       onTap: () {
+  //         AssetsAudioPlayer.newPlayer().open(
+  //           Audio("assets/notes/note_1.wav"),
+  //           autoStart: true,
+  //           showNotification: true,
+  //         );
+  //         // final AudioPlayer audioPlayer = AudioPlayer();
+  //         // audioPlayer.play('assets/notes/note_1.wav');
+  //       },
+  //       child: Container(
+  //         width: double.infinity,
+  //         color: containerTus,
+  //         child: Text(
+  //           'Note:$text ',
+  //           style: TextStyle(
+  //             fontSize: 30,
+  //             color: textColor,
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+// aty jok - birok suragan parametrdi berbei koisok dele ishtei beret
+  // Expanded newMethod3([Color? containerTus, String? text, Color? textColor]) {
+  //   return Expanded(
+  //     child: InkWell(
+  //       onTap: () {
+  //         AssetsAudioPlayer.newPlayer().open(
+  //           Audio("assets/notes/note_1.wav"),
+  //           autoStart: true,
+  //           showNotification: true,
+  //         );
+  //         // final AudioPlayer audioPlayer = AudioPlayer();
+  //         // audioPlayer.play('assets/notes/note_1.wav');
+  //       },
+  //       child: Container(
+  //         width: double.infinity,
+  //         color: containerTus,
+  //         child: Text(
+  //           'Note:$text ',
+  //           style: TextStyle(
+  //             fontSize: 30,
+  //             color: textColor,
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+}
+
+class NotaWidget extends StatelessWidget {
+  final Color? containerTus;
+
+  const NotaWidget({
+    this.containerTus,
+  });
+  // newMethod2(Color? containerTus,  )
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          AssetsAudioPlayer.newPlayer().open(
+            Audio("assets/notes/note_2.wav"),
+            autoStart: true,
+            showNotification: true,
+          );
+          // final AudioPlayer audioPlayer = AudioPlayer();
+          // audioPlayer.play('assets/notes/note_1.wav');
+        },
+        child: Container(
+          width: double.infinity,
+          color: containerTus,
+          child: const Text(
+            'Note: misal ',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
